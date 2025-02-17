f31ccb3e535f38e29cff99b154f6cacea731d5a1
waleedkhaled <waleedo1435@gmail.com>
2025-02-17 14:30:34 +0000

    Fix routing configuration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import { Routes, Route } from 'react-router-dom';
 import ExcelForm from './components/ExcelForm';
 import TotalPointsPage from './components/TotalPointsPage';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/add-points" element={<ExcelForm />} />
-          <Route path="/total-points" element={<TotalPointsPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<ExcelForm />} />
+      <Route path="/total-points" element={<TotalPointsPage />} />
+    </Routes>
   );
 }
 
