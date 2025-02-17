24c066e93c5e0648c756d9609362703782349653
waleedkhaled <waleedo1435@gmail.com>
2025-02-17 14:37:32 +0000

    updating app.js to route to homepage first

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 // src/App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 import ExcelForm from './components/ExcelForm';
 import TotalPointsPage from './components/TotalPointsPage';
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ExcelForm />} />
-      <Route path="/total-points" element={<TotalPointsPage />} />
-    </Routes>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/add-points" element={<ExcelForm />} />
+        <Route path="/total-points" element={<TotalPointsPage />} />
+      </Routes>
+    </div>
   );
 }
 
